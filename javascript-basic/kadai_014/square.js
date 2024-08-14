caa83001126eb33dd04fb2630d18fe0d14dd294a
kareni136 <jsfwm206@gmail.com>
2024-08-14 10:47:20 +0900

    kadai_014

---
+++ javascript-basic/kadai_014/square.js
@@ -0,0 +1,14 @@
+// // アロー関数で定義
+// const total = (i) => i * i;
+
+// // 関数の呼び出しとコンソールの出力
+// console.log(total(10));
+
+
+// functionで定義
+function total(i) {
+  return i * i;
+}
+
+// 関数の呼び出しとコンソールの出力
+console.log(total(10));
